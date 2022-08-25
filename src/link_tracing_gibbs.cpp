f8e8f9eb7b011bc54e9ffb359516743ec56ebf07
till-tietz <ttietz2014@gmail.com>
2022-08-25 03:50:55 +0200

    updating data structures

--- src/link_tracing_gibbs.cpp
+++ src/link_tracing_gibbs.cpp
@@ -200,158 +200,168 @@ List lt_gibbs(DataFrame data,
   std::vector<double> prior_l = priors["p_l"];
   int prior_b = priors["p_b"];
   
-  int t = 1;
-  
   // begin MCMC
-  //  for(int t = 1; t < chain_samples; t++){
-  
-  //##################
-  //# generate new N #
-  //##################
-  
-  //get number of units in each strata
-  IntegerVector data_p_strata = data_p["strata"];
-  IntegerVector rows = c_unlist(head(data_p_waves, n_waves - 1));
-  IntegerVector rows_pull = rows - 1;
-  
-  IntegerVector strata_t(rows_pull.size());
-  
-  for(int i = 0; i < rows_pull.size(); i++){
-    strata_t[i] = data_p_strata[rows_pull[i]];
-  }
-  
-  IntegerVector strata_count = table(strata_t);
-  
-  //get p(no link between strata
-  NumericVector one = {1};
-  NumericVector no_link_init =  rep(one,n_strata);
-  
-  for(int i = 0; i < n_strata; i++){
-    for(int j = 0; j < n_strata; j++){
-      NumericMatrix bi = b[t - 1];
-      no_link_init[i] = no_link_init[i] * pow((1 - bi(j,i)),strata_count[j]);
+  for(int t = 1; t < chain_samples; t++){
+    
+    //##################
+    //# generate new N #
+    //##################
+    
+    //get number of units in each strata
+    std::vector<int> data_p_strata = data["strata"];
+    
+    std::vector<int> strata_t;
+    
+    for(int i = 0; i < data_p_waves.size(); i++){
+      std::vector<int> strata_ti;
+      
+      for(int j = 0; j < data_p_waves[i].size(); j ++){
+        strata_ti.push_back(data_p_strata[data_p_waves[i][j] - 1]);
+      }
+      
+      strata_t.insert(strata_t.end(), strata_ti.begin(), strata_ti.end());
     }
+    
+    std::vector<int> strat(strata_t);
+    sort(strat.begin(),strat.end());
+    strat.erase(unique(strat.begin(),strat.end() ),strat.end());
+    
+    std::vector<int> strata_count;
+    
+    for(int i = 0; i < strat.size(); i++){
+      strata_count.push_back(std::count(strata_t.begin(),strata_t.end(), strat[i]));
+    }
+    
+    //get p(no link between strata
+    NumericVector one = {1};
+    NumericVector no_link_init =  rep(one,n_strata);
+    
+    for(int i = 0; i < n_strata; i++){
+      for(int j = 0; j < n_strata; j++){
+        NumericMatrix bi = b[t - 1];
+        no_link_init[i] = no_link_init[i] * pow((1 - bi(j,i)),strata_count[j]);
+      }
+    }
+    
+    NumericVector no_link_l = as<NumericVector>(l[t-1]) * no_link_init;
+    double no_link = sum(no_link_l);
+    
+    int nn_0 = as<IntegerVector>(data_p_waves[0]).size();
+    int nn = as<IntegerVector>(c_unlist(data_p_waves)).size();
+    
+    IntegerVector n_post_range = Range(nn, total * 5);
+    
+    NumericVector n_sample_prob_vec(n_post_range.size());
+    
+    for(int i = 0; i < n_post_range.size(); i++){
+      IntegerVector r_i = Range(n_post_range[i] + 1 - nn, n_post_range[i] - nn_0);
+      n_sample_prob_vec[i] =  sum(log(r_i)) +
+        (n_post_range[i] - nn) * log(no_link) - prior_n * log(n_post_range[i]);
+    }
+    
+    NumericVector n_sample_prob = exp(n_sample_prob_vec - max(n_sample_prob_vec));
+    n[t] = sample(n_post_range, 1, false, n_sample_prob)[0];
+    
+    
+    //#######################
+    //# generate new lambda #
+    //#######################
+    
+    // assign strata to non sampled units
+    
+    // get indices of non sampled units
+    IntegerVector n_range = Range(1, n[t]);
+    IntegerVector not_sampled = setdiff(n_range, as<IntegerVector>(c_unlist(data_p_reorder))) - 1;
+    
+    IntegerVector stratum(n[t]);
+    
+    //fill stratum vector with strata of sampled units
+    IntegerVector ins_pos_us = as<IntegerVector>(c_unlist(data_p_reorder));
+    ins_pos_us = ins_pos_us - 1;
+    rows_pull = as<IntegerVector>(c_unlist(data_p_waves));
+    rows_pull = rows_pull - 1;
+    IntegerVector ins_val_us(rows_pull.size());
+    
+    for(int i = 0; i < rows_pull.size(); i++){
+      ins_val_us[i] = data_p_strata[rows_pull[i]];
+    }
+    
+    stratum = int_vec_insert(as<IntegerVector>(stratum),
+                             as<IntegerVector>(ins_val_us),
+                             as<IntegerVector>(ins_pos_us));
+    
+    //fill stratum vector with strata of non sampled units
+    IntegerVector strat = Range(1,n_strata);
+    NumericVector pstrat = no_link_l / no_link;
+    IntegerVector stratsamp = sample(as<IntegerVector>(strat), not_sampled.size(),
+                                     true, as<NumericVector>(pstrat));
+    
+    stratum = int_vec_insert(as<IntegerVector>(stratum),
+                             as<IntegerVector>(stratsamp),
+                             as<IntegerVector>(not_sampled));
+    
+    
+    
+    // populate link matrix for reordered sample
+    DataFrame link_comb = c_expand_grid(Range(0,n_waves - 1), Range(0,n_waves - 1));
+    IntegerVector g1 = link_comb[0];
+    IntegerVector g2 = link_comb[1];
+    
+    IntegerMatrix y(n[t],n[t]);
+    
+    for(int i = 0; i < g1.size() - 1; i++){
+      y = mat_to_mat_insert(y_samp,
+                            y,
+                            data_p_reorder[g1[i]],
+                                          data_p_reorder[g2[i]],
+                                                        data_p_waves[g1[i]],
+                                                                    data_p_waves[g2[i]]);
+    }
+    
+    IntegerVector lp_1 = c_unlist(data_p_reorder[Range(0,data_p_reorder.size() - 2)]);
+    IntegerVector lp_2 = Range(1,n[t]);
+    IntegerMatrix link_pairs =  c_combn(c_setdiff(lp_2,lp_1),2);
+    link_pairs  = transpose(link_pairs);
+    
+    int n_pairs = link_pairs.nrow();
+    NumericVector link_prob = runif(n_pairs);
+    
+    IntegerVector assigned = c_which(link_prob < mat_by_mat(b[t],
+                                                            stratum[link_pairs(_,0)],
+                                                                   stratum[link_pairs(_,1)]));
+    
+    IntegerVector link_pairs_0 = link_pairs(_,0);
+    IntegerVector link_pairs_1 = link_pairs(_,1);
+    IntegerVector vals (link_pairs_0.length(),1);
+    
+    y = int_mat_insert(y,
+                       link_pairs_1[assigned - 1],
+                                   link_pairs_0[assigned - 1],
+                                               vals);
+    
+    y = int_mat_insert(y,
+                       link_pairs_0[assigned - 1],
+                                   link_pairs_0[assigned - 1],
+                                               vals);
+    
+    
+    // new lambda
+    NumericVector strata_count_num = as<NumericVector>(table(stratum));
+    IntegerVector strate_count_int = table(stratum);
+    NumericMatrix dirichlet = c_rdirichlet(1, strata_count_num + prior_l);
+    l(t) = dirichlet(0,_);
+    
+    //#####################
+    //# generate new beta #
+    //#####################
+    
+    
+    
   }
   
-  NumericVector no_link_l = as<NumericVector>(l[t-1]) * no_link_init;
-  double no_link = sum(no_link_l);
-  
-  int nn_0 = as<IntegerVector>(data_p_waves[0]).size();
-  int nn = as<IntegerVector>(c_unlist(data_p_waves)).size();
-  
-  IntegerVector n_post_range = Range(nn, total * 5);
-  
-  NumericVector n_sample_prob_vec(n_post_range.size());
-  
-  for(int i = 0; i < n_post_range.size(); i++){
-    IntegerVector r_i = Range(n_post_range[i] + 1 - nn, n_post_range[i] - nn_0);
-    n_sample_prob_vec[i] =  sum(log(r_i)) +
-      (n_post_range[i] - nn) * log(no_link) - prior_n * log(n_post_range[i]);
-  }
-  
-  NumericVector n_sample_prob = exp(n_sample_prob_vec - max(n_sample_prob_vec));
-  n[t] = sample(n_post_range, 1, false, n_sample_prob)[0];
-  
-  
-  //#######################
-  //# generate new lambda #
-  //#######################
-  
-  // assign strata to non sampled units
-  
-  // get indices of non sampled units
-  IntegerVector n_range = Range(1, n[t]);
-  IntegerVector not_sampled = setdiff(n_range, as<IntegerVector>(c_unlist(data_p_reorder))) - 1;
-  
-  IntegerVector stratum(n[t]);
-  
-  //fill stratum vector with strata of sampled units
-  IntegerVector ins_pos_us = as<IntegerVector>(c_unlist(data_p_reorder));
-  ins_pos_us = ins_pos_us - 1;
-  rows_pull = as<IntegerVector>(c_unlist(data_p_waves));
-  rows_pull = rows_pull - 1;
-  IntegerVector ins_val_us(rows_pull.size());
-  
-  for(int i = 0; i < rows_pull.size(); i++){
-    ins_val_us[i] = data_p_strata[rows_pull[i]];
-  }
-  
-  stratum = int_vec_insert(as<IntegerVector>(stratum),
-                           as<IntegerVector>(ins_val_us),
-                           as<IntegerVector>(ins_pos_us));
-  
-  //fill stratum vector with strata of non sampled units
-  IntegerVector strat = Range(1,n_strata);
-  NumericVector pstrat = no_link_l / no_link;
-  IntegerVector stratsamp = sample(as<IntegerVector>(strat), not_sampled.size(),
-                                   true, as<NumericVector>(pstrat));
-  
-  stratum = int_vec_insert(as<IntegerVector>(stratum),
-                           as<IntegerVector>(stratsamp),
-                           as<IntegerVector>(not_sampled));
-  
-  
-  
-  // populate link matrix for reordered sample
-  DataFrame link_comb = c_expand_grid(Range(0,n_waves - 1), Range(0,n_waves - 1));
-  IntegerVector g1 = link_comb[0];
-  IntegerVector g2 = link_comb[1];
-  
-  IntegerMatrix y(n[t],n[t]);
-  
-  for(int i = 0; i < g1.size() - 1; i++){
-    y = mat_to_mat_insert(y_samp,
-                          y,
-                          data_p_reorder[g1[i]],
-                                        data_p_reorder[g2[i]],
-                                                      data_p_waves[g1[i]],
-                                                                  data_p_waves[g2[i]]);
-  }
-  
-  IntegerVector lp_1 = c_unlist(data_p_reorder[Range(0,data_p_reorder.size() - 2)]);
-  IntegerVector lp_2 = Range(1,n[t]);
-  IntegerMatrix link_pairs =  c_combn(c_setdiff(lp_2,lp_1),2);
-  link_pairs  = transpose(link_pairs);
-  
-  int n_pairs = link_pairs.nrow();
-  NumericVector link_prob = runif(n_pairs);
-  
-  IntegerVector assigned = c_which(link_prob < mat_by_mat(b[t],
-                                                          stratum[link_pairs(_,0)],
-                                                                 stratum[link_pairs(_,1)]));
-  
-  IntegerVector link_pairs_0 = link_pairs(_,0);
-  IntegerVector link_pairs_1 = link_pairs(_,1);
-  IntegerVector vals (link_pairs_0.length(),1);
-  
-  y = int_mat_insert(y,
-                     link_pairs_1[assigned - 1],
-                                 link_pairs_0[assigned - 1],
-                                             vals);
-  
-  y = int_mat_insert(y,
-                     link_pairs_0[assigned - 1],
-                                 link_pairs_0[assigned - 1],
-                                             vals);
-  
-  
-  // new lambda
-  NumericVector strata_count_num = as<NumericVector>(table(stratum));
-  IntegerVector strate_count_int = table(stratum);
-  NumericMatrix dirichlet = c_rdirichlet(1, strata_count_num + prior_l);
-  l(t) = dirichlet(0,_);
-  
-  //#####################
-  //# generate new beta #
-  //#####################
-  
-  
-  
-}
-
-List ret(3);
-ret[0] = data_p_reorder;
-ret[1] = data_p_waves;
-ret[2] = ins_pos_us;
-return ret;
+  List ret(3);
+  ret[0] = data_p_reorder;
+  ret[1] = data_p_waves;
+  ret[2] = ins_pos_us;
+  return ret;
 }
